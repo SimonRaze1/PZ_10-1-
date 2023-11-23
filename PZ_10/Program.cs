23a1002877a3c49330121b5913dd1fd7683daf2b
User <User@DESKTOP-AMGP8UK>
2023-11-23 08:45:43 +0500

    Добавьте файлы проекта.

---
+++ PZ_10/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите текст:");
+        string input = Console.ReadLine();
+        string output = ModifyText(input);
+        Console.WriteLine("Модифицированный текст:");
+        Console.WriteLine(output);
+    }
+
+    static string ModifyText(string input)
+    {
+        char[] chars = input.ToCharArray();
+        for (int i = 0; i < chars.Length; i++)
+        {
+            if (char.IsLetter(chars[i]))
+            {
+                if (char.IsLower(chars[i]))
+                {
+                    chars[i] = char.ToUpper(chars[i]);
+                }
+                else
+                {
+                    chars[i] = char.ToLower(chars[i]);
+                }
+            }
+        }
+        return new string(chars);
+    }
+}
+
